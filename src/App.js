1ba76876a309d11a03b031d6c6854034c9f6e3f4
IT21356704 <it21356704@my.sliit.lk>
2024-08-15 16:05:17 +0530

    Live chat started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Features from './sections/Features';
 import Header from './sections/Header';
+import LiveChat from './sections/LiveChat';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Features />
+      <LiveChat />
       
     </div>
   );
